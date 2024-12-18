5cfd7dbcc18c43747ff794d74ceaf50a70cef382
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-18 14:17:29 +0700

    Membuat Entity data barang dan suplier

---
+++ app/src/main/java/com/example/ucp2/data/entity/Barang.kt
@@ -0,0 +1,26 @@
+package com.example.ucp2.data.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "barang",
+        foreignKeys = [
+            ForeignKey(
+                entity = Suplier::class,
+                parentColumns = ["nama"],
+                childColumns = ["namaSuplier"],
+                onDelete = ForeignKey.CASCADE
+            )
+        ]
+)
+
+data class Barang(
+    @PrimaryKey
+    val id: String,
+    val nama: String,
+    val deskripsi: String,
+    val harga: String,
+    val stok: String,
+    val namaSuplier: String
+)
