546a6086c271a387fff8af313112cc78800aa273
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-18 14:28:00 +0700

    Membuat database Inventaris dengan fungsi barang dan suplier dao

---
+++ app/src/main/java/com/example/ucp2/data/database/InventarisDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.ucp2.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.ucp2.data.dao.BarangDao
+import com.example.ucp2.data.dao.SuplierDao
+import com.example.ucp2.data.entity.Barang
+import com.example.ucp2.data.entity.Suplier
+
+@Database(entities = [Barang::class, Suplier::class], version = 1, exportSchema = false)
+abstract class InventarisDatabase : RoomDatabase(){
+    abstract fun barangDao(): BarangDao
+    abstract fun suplierDao(): SuplierDao
+
+    companion object {
+        @Volatile
+        private var Instance: InventarisDatabase? = null
+
+        fun getDatabase(context: Context): InventarisDatabase {
+            return (Instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context,
+                    InventarisDatabase::class.java,
+                    "InventarisDatabase"
+                )
+                    .build().also { Instance = it }
+            })
+        }
+    }
+}
