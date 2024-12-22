0e5e6aca1f03649a92ab954ef780c8f9e3823c14
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-22 20:57:16 +0700

    Menambahkan data class DetailBrgUiState berisi variabel-variabel yang dibutuhkan

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailBrgViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailBrgViewModel.kt
@@ -2,6 +2,19 @@ package com.example.ucp2.ui.viewmodel
 
 import com.example.ucp2.data.entity.Barang
 
+data class DetailBrgUiState(
+    val detailUiEvent: BarangEvent = BarangEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+) {
+    val isUiEventEmpty: Boolean
+        get() = detailUiEvent == BarangEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != BarangEvent()
+}
+
 fun Barang.toDetailBrgUiEvent(): BarangEvent {
     return BarangEvent(
         id = id,
