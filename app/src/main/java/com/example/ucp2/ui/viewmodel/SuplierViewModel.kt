105abe4ea2caf16108fb10f6118644b106d35d00
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-22 11:59:30 +0700

    membuat fungsi toSuplierEntity untuk menyimpan nilai data event ke entity Suplier

--- app/src/main/java/com/example/ucp2/ui/viewmodel/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/SuplierViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.Suplier
+
+
+fun SuplierEvent.toSuplierEntity(): Suplier = Suplier (
+    id = id,
+    nama = nama,
+    kontak = kontak,
+    alamat = alamat,
+)
 
 data class SuplierEvent(
     val id: Int = 0,
