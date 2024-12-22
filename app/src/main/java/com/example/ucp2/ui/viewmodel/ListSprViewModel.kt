235cded97e520e87929cb2a8a33fbf47e2a27259
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-22 12:27:58 +0700

    Membuat class ListSprViewModel untuk mengelola list suplier

--- app/src/main/java/com/example/ucp2/ui/viewmodel/ListSprViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/ListSprViewModel.kt
@@ -1,6 +1,51 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Suplier
+import com.example.ucp2.repository.RepositorySpr
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+
+class ListSprViewModel(
+    private val repositorySpr: RepositorySpr
+) : ViewModel() {
+    val listSprUiState: StateFlow<ListSprUiState> = repositorySpr.getAllSpr()
+        .filterNotNull()
+        .map {
+            ListSprUiState(
+                listSpr = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(ListSprUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                ListSprUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ListSprUiState(
+                isLoading = true,
+            )
+        )
+}
+
 
 data class ListSprUiState(
     val listSpr: List<Suplier> = listOf(),
