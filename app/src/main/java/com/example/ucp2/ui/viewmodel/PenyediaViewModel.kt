835d7be36db049684beae7d336e2331296604ed1
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-22 21:53:10 +0700

    Menambahkan videmodel Barang

--- app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -1,12 +1,38 @@
 package com.example.ucp2.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucp2.InventarisApp
 
 object PenyediaViewModel {
+
+    val Factory = viewModelFactory {
+        initializer {
+            BarangViewModel(
+                inventarisApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            ListBrgViewModel(
+                inventarisApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            DetailBrgViewModel(
+                createSavedStateHandle(),
+                inventarisApp().containerApp.repositoryBrg
+            )
+        }
+        initializer {
+            UpdateBrgViewModel(
+                createSavedStateHandle(),
+                inventarisApp().containerApp.repositoryBrg
+            )
+        }
+    }
 
     val Factory1 = viewModelFactory {
         initializer {
