d6cb147586b245345f0d1c210f37bc91bcc52921
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-22 12:02:56 +0700

    Membuat data class FormErrorStatespr berupa variabel untuk mengecek nilai null

--- app/src/main/java/com/example/ucp2/ui/viewmodel/SuplierViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/SuplierViewModel.kt
@@ -2,6 +2,17 @@ package com.example.ucp2.ui.viewmodel
 
 import com.example.ucp2.data.entity.Suplier
 
+data class FormErrorStatespr(
+    val id: String? = null,
+    val nama: String? = null,
+    val kontak: String? = null,
+    val alamat: String? = null,
+) {
+    fun isValid(): Boolean {
+        return id == null && nama == null && kontak == null &&
+                alamat  == null
+    }
+}
 
 fun SuplierEvent.toSuplierEntity(): Suplier = Suplier (
     id = id,
