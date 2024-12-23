831837d6d3c74c53a95afcff5f0323acf6bc22a9
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 11:56:42 +0700

    Menambahkan Icon dalam pengkondisian, judul, dan subjudul

--- app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
@@ -1,7 +1,9 @@
 package com.example.ucp2.ui.customwidget
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -17,7 +20,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TopAppBar(
@@ -55,6 +60,36 @@ fun TopAppBar(
                     Text("Kembali")
                 }
                 Spacer(modifier = Modifier.weight(2f))
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Column (
+                modifier = Modifier.weight(1f)
+            ) {
+                if (showIcon) {
+                    Icon(
+                        imageVector = Icons.Filled.Menu,
+                        contentDescription = "",
+                        tint = Color.White,
+                        modifier = Modifier
+                    )
+                }
+                Text(
+                    modifier = Modifier.padding(top = 50.dp),
+                    text = judul,
+                    fontSize = 25.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                )
+                Text(
+                    text = subjudul,
+                    fontSize = 15.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Normal,
+                )
             }
         }
     }
