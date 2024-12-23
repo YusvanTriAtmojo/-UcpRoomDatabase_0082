61138726e84943dba068ac6ff3c91b9a005420a9
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 15:04:08 +0700

    Membuat fungsi ComponentDetailBrg berupa text dengan nilai parameter judul dan isinya

--- app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
@@ -1,10 +1,40 @@
 package com.example.ucp2.ui.view.barang
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ComponentDetailBrg(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Column (
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "$judul :",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
 
 @Composable
 private fun DeleteConfirmationDialog(
