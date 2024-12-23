474be99e5f8d0f265c55a06be9450b42eb27cb31
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 14:51:26 +0700

    Membuat fungsi utama insert barang view yang berisi tampilan TopAppBar dan Card fungsi InsertBodyBrg

--- app/src/main/java/com/example/ucp2/ui/view/barang/InsertBrgView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/InsertBrgView.kt
@@ -1,9 +1,12 @@
 package com.example.ucp2.ui.view.barang
 
 import DynamicSelectedTextField
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -15,10 +18,18 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,11 +37,75 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.data.NamaSuplier
 import com.example.ucp2.data.entity.Suplier
+import com.example.ucp2.ui.customwidget.TopAppBar
 import com.example.ucp2.ui.viewmodel.BarangEvent
+import com.example.ucp2.ui.viewmodel.BarangViewModel
 import com.example.ucp2.ui.viewmodel.BrgUIState
 import com.example.ucp2.ui.viewmodel.FormErrorStatebrg
+import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertBrgView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: BarangViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { padding ->
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(Color(0xFF01A58B))
+        ){
+
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                showIcon = false,
+                showImage = false,
+                judul = "Tambah Barang",
+                subjudul = "",
+                modifier = modifier
+            )
+
+            Card ( modifier = modifier.height(10000.dp).padding(start = 15.dp, end = 15.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.White)){
+                InsertBodyBrg(
+                    uiState = uiState,
+                    onValueChange = { updateEvent ->
+                        viewModel.updateBrgState(updateEvent)
+                    },
+                    onClick = {
+                        viewModel.saveData()
+                        onNavigate()
+                    }
+                )
+            }
+        }
+    }
+}
 
 @Composable
 fun InsertBodyBrg(
