06df02d5c301c0e505e940c8e6aea4b4b40a2eab
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 12:14:58 +0700

    Membuat bagian isi di dalam column terdapat card dengan Text Menu Utama

--- app/src/main/java/com/example/ucp2/ui/view/HomeView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HomeView.kt
@@ -1,10 +1,22 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ucp2.ui.customwidget.TopAppBar
 
 
@@ -27,7 +39,31 @@ fun HomeView(
                 modifier = Modifier
             )
         }
-    ) {
-
+    ) { innerPadding ->
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        )
+        {
+            Card(
+                modifier = Modifier
+                    .padding(top = 10.dp, start = 10.dp, end = 10.dp)
+                    .height(450.dp)
+                    .width(500.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFF01A58B)),
+            )
+            {
+                Text(
+                    modifier = Modifier.padding(start = 120.dp, top = 20.dp),
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 30.sp,
+                    color = Color.White,
+                    text = "Menu Utama"
+                )
+            }
+        }
     }
 }
