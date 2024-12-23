78b873203c4177f8dcba0fe4e997e294b748401b
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 15:10:58 +0700

    Membuat fungsi ItemDetailBrg berupa card dengan column yang menginisiasikan nilai dari judul dan isinya

--- app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/barang/DetailBrgView.kt
@@ -2,15 +2,53 @@ package com.example.ucp2.ui.view.barang
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Barang
+
+@Composable
+fun ItemDetailBrg(
+    modifier: Modifier = Modifier,
+    barang: Barang
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFF018786),
+            contentColor = Color.White,
+        )
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailBrg(judul = "ID", isinya = barang.id)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailBrg(judul = "Nama", isinya = barang.nama)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Deskripsi", isinya = barang.deskripsi)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Harga", isinya = barang.harga.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Stok", isinya = barang.stok.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailBrg(judul = "Nama Suplier", isinya = barang.namaSuplier)
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailBrg(
