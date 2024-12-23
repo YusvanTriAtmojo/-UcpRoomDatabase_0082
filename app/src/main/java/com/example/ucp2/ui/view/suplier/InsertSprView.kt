fb5f3e82bbf8d043cfe5d1e2acab45ba5e0a7bbc
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 13:28:36 +0700

    Membuat fungsi body insert suplier dengan Column yang berisi fungsi FormSuplier dan tombol simpan

--- app/src/main/java/com/example/ucp2/ui/view/suplier/InsertSprView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/suplier/InsertSprView.kt
@@ -1,17 +1,22 @@
 package com.example.ucp2.ui.view.suplier
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
@@ -19,7 +24,40 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ucp2.ui.viewmodel.FormErrorStatespr
+import com.example.ucp2.ui.viewmodel.SprUIState
 import com.example.ucp2.ui.viewmodel.SuplierEvent
+
+@Composable
+fun InsertBodySpr(
+    modifier: Modifier = Modifier,
+    onValueChange: (SuplierEvent) -> Unit,
+    uiState: SprUIState,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormSuplier(
+            suplierEvent = uiState.suplierEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button (
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF016D47),
+                contentColor = Color.White
+            )
+        ) {
+            Text("Simpan")
+
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
