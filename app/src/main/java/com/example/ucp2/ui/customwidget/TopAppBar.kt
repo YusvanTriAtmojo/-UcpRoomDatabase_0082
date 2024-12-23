13cb471170e8403199bb332b6a44a0b0c87fbe88
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2024-12-23 12:01:25 +0700

    Menambahkan gambar atk ke drawable dan menambahkannya di TopAppBar dalam pengkondisian

--- app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/customwidget/TopAppBar.kt
@@ -1,5 +1,6 @@
 package com.example.ucp2.ui.customwidget
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -20,9 +22,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.R
 
 @Composable
 fun TopAppBar(
@@ -89,6 +93,16 @@ fun TopAppBar(
                     fontSize = 15.sp,
                     color = Color.White,
                     fontWeight = FontWeight.Normal,
+                )
+            }
+            if (showImage) {
+                Image(
+                    painter = painterResource(id = R.drawable.atk),
+                    contentDescription = "",
+                    modifier = Modifier
+                        .size(150.dp)
+                        .padding(end = 30.dp, top = 10.dp)
+
                 )
             }
         }
